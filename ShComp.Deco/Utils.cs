e8b3bb9cce2c449e1a56c6ae34d54257802bc857
ShTair <git@vsh.jp>
2023-10-20 17:04:53 +0900

    名前を変換するコードを追加

---
+++ ShComp.Deco/Utils.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace ShComp.Deco;
+
+public static class Utils
+{
+    public static string? DecodeName(string? name) => name is null ? null : DecodeName(Convert.FromBase64String(name));
+
+    public static string? DecodeName(byte[]? nameData) => nameData is null ? null : Encoding.UTF8.GetString(nameData);
+}
